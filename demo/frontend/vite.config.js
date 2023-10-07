ab35e82c35211c6213b67af8e1113eb70ac54c06
Vinícius Campitelli <1877191+vcampitelli@users.noreply.github.com>
2023-10-07 10:37:57 -0300

    :heavy_minus_sign: Removendo vite-plugin-mkcert

--- demo/frontend/vite.config.js
+++ demo/frontend/vite.config.js
@@ -1,16 +1,9 @@
 import {defineConfig} from 'vite';
 import preact from '@preact/preset-vite';
-import mkcert from 'vite-plugin-mkcert';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    // server: {https: true},
     plugins: [
         preact(),
-        // mkcert({
-        //     hosts: [
-        //         'webauthn.local'
-        //     ]
-        // }),
     ],
 });
